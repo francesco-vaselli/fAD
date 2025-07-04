0c3dc407d3b3b218666076856ed71a1538ca8fad
Francesco Vaselli <francesco.vaselli@protonmail.com>
2025-07-04 16:11:34 +0200

    switch exp

--- experiments/hyperparameter_search.py
+++ experiments/hyperparameter_search.py
@@ -73,7 +73,7 @@ keys, values = zip(*param_grid_custom.items())
 experiments_custom = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
 # Combine all experiments
-experiments = experiments_mlp  # + experiments_custom
+experiments = experiments_custom  # + experiments_mlp
 
 all_results = []
 best_model_score = -1
